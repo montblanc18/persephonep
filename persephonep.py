d58a92fc0116f153537285c94cd189038fa80b61
montblanc18 <montblanc.u3.07@gmail.com>
2018-04-24 17:51:51 +0900

    delete mainui.

--- persephonep.py
+++ persephonep.py
@@ -10,72 +10,6 @@ from PyQt5.QtCore import pyqtSlot
 
 __program__ = 'PERSEPHONEP'
 
-
-class mainUI(QWidget):
-    ''' Main UI of PERSEPHONE
-    '''
-
-    def __init__(self):
-        super(mainUI, self).__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.add_button = QPushButton('+')
-        self.add_button.setStyleSheet('background-color:gray')
-        self.add_button.clicked.connect(self._addTab)
-        self.app_info = QLabel('PERSEPHONE is developed by @montblanc18. This is a Web Browser based on Python 3 and PyQt5.')
-        
-        
-        self.qtab = QTabWidget()
-        self.qtab.setTabsClosable(True);
-        self._addTab()
-
-        self.layout = QGridLayout()
-        self.layout.addWidget(self.add_button, 0, 0)
-        self.layout.addWidget(self.app_info, 0, 1)
-        self.layout.addWidget(self.qtab, 1,0,6,16)
-        self.setLayout(self.layout)
-        
-        '''
-        self.hbox = QHBoxLayout()
-        self.hbox.addWidget(self.add_button)
-        self.hbox.addWidget(self.author_label)
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.hbox)
-        self.vbox.addWidget(self.qtab)
-        
-        self.setLayout(vbox)
-        '''
-
-        self.resize(1200, 800)
-        self.center()
-        self.setWindowTitle(__program__)
-        self.show()
-        
-    def _addTab(self):
-        ''' add Tab
-        '''
-        self.qtab.addTab(PersephoneWindow(parent = self), '')
-        self.qtab.tabCloseRequested.connect(self.closeTab)
-
-    def closeTab(self, index):
-        ''' close Tab. This function has some problem.
-        when this is called, some tabs was deleted together.
-        '''
-        widget = self.qtab.widget(index)
-        #if widget is not None:
-        #    widget.deleteLater()
-        self.qtab.removeTab(index)
-        
-    def center(self):
-        ''' centering widget
-        '''
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-
 class PersephoneMainWidget(QMainWindow):
 
     def __init__(self):
@@ -172,6 +106,5 @@ if __name__ == '__main__':
     app.setWindowIcon(QIcon(path))
 
     
-    # ui = mainUI()
     ui = PersephoneMainWidget()
     sys.exit(app.exec_())
