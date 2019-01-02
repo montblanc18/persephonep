f8c580c2cfdbbaee77905ccc403a79466fe81115
montblanc18 <skurita.phys@gmail.com>
2019-01-02 23:49:43 +0900

    Add tests, but it is not perfect.

---
+++ tests/test_func_persephonep.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+
+import unittest
+from func_persephonep import *
+
+
+class TestFuncPersephonep(unittest.TestCase):
+
+    def setUp(self):
+        return
