57b8cf4ef3ab981ca0a2dfe592148c5e71e6f0a0
montblanc18 <montblanc.u3.07@gmail.com>
2019-03-22 17:02:41 +0900

    Commit before make new branches.

--- tests/test_persephonep.py
+++ tests/test_persephonep.py
@@ -7,7 +7,7 @@ from persephonep import *
 import PyQt5.QtCore
 
 
-def test_perseponep(qtbot):
+def test_add_button(qtbot):
     app = QApplication(sys.argv)
 
     # setWindowIcon is a method for QApplication, not for QWidget
@@ -16,11 +16,32 @@ def test_perseponep(qtbot):
 
     ui = PersephonepMainWidget()
 
+    qWarning('test')
+
     qtbot.addWidget(ui)
     qtbot.mouseClick(ui.table_widget.add_button, PyQt5.QtCore.Qt.LeftButton)
     assert ui.table_widget.add_button.text() == '+'
 
-'''
+
+
+''''
+def test_check_url_protocol():
+
+    # mainPyQt5()
+    app = QApplication(sys.argv)
+
+    # setWindowIcon is a method for QApplication, not for QWidget
+    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'icon_persephone.png')
+    app.setWindowIcon(QIcon(path))
+
+    ex = PersephonepWindow()
+
+    # assert ex.check_url_protocol('http://127.0.0.1') == True
+    #assert ex.check_url_protocol('https://127.0.0.1') == True
+    #assert ex.check_url_protocol('ftp://127.0.0.1') == True
+    #assert ex.check_url_protocol('file://127.0.0.1') == True
+
+
 class TestPersephonep(unittest.TestCase):
 
     def setUp(self):
