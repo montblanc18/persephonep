06d874dabb91eeae3e4699307ebf418417f97a2b
montblanc18 <montblanc.u3.07@gmail.com>
2019-03-23 11:07:18 +0900

    Modify persephonep.py and func_persephonep.py to obey PEP8.

--- persephonep.py
+++ persephonep.py
@@ -3,9 +3,14 @@
 #
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QTabWidget, QApplication, QHBoxLayout, QVBoxLayout, QLabel, QGridLayout
+import os
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QPushButton,
+                             QTabWidget, QApplication,
+                             # QHBoxLayout, QGridLayout,
+                             QVBoxLayout, QLabel, QDesktopWidget)
 from PyQt5.QtGui import QIcon
-from func_persephonep import *
+# from func_persephonep import *
+from func_persephonep import PersephonepWindow
 from PyQt5.QtCore import pyqtSlot
 
 __program__ = 'PERSEPHONEP'
@@ -13,6 +18,8 @@ __program__ = 'PERSEPHONEP'
 ''' This is a main window of the browser.
     This class includes PersephonepMainWidget.
 '''
+
+
 class PersephonepMainWidget(QMainWindow):
 
     def __init__(self):
@@ -34,41 +41,43 @@ class PersephonepMainWidget(QMainWindow):
     This class is called by PersephonepMainWindow,
     and it has some PersephonepWindos.
 '''
+
+
 class PersephonepTableWidget(QWidget):
 
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
 
-        
         # initialize tab screen
         self.tabs = QTabWidget()
-        # self.tab1 = QWidget()
-        # self.tab2 = QWidget()
-        self.tab = [] # Store the PersephonepWindow Class
+        #  self.tab1 = QWidget()
+        #  self.tab2 = QWidget()
+        self.tab = []  # Store the PersephonepWindow Class
         self.tabs.resize(1200, 800)
 
         # Add Tabs
-        self._addTab(len(self.tab)) # len(self.tab) => 0
+        self._addTab(len(self.tab))  # len(self.tab) => 0
         # self.tabs.addTab(self.tab1, 'Tab 1')
         # self.tabs.addTab(self.tab2, 'Tab 2')
         self.add_button = QPushButton('+')
         self.add_button.setStyleSheet('background-color:gray')
-        self.add_button.clicked.connect(lambda: self._addTab(len(self.tab))) # add tab to last of index
-        self.app_info = QLabel('%s is a Web Browser based on Python 3 and PyQt5, developed by @montblanc18.' % __program__)
+        # add tab to last of index
+        self.add_button.clicked.connect(lambda: self._addTab(len(self.tab)))
+        app_info_text = '%s is a Web Browser based on Python 3 and PyQt5,' \
+                        ' developed by @montblanc18.' % __program__
+        self.app_info = QLabel(app_info_text)
 
         # define the delete tab process
         self.tabs.setTabsClosable(True)
         self.tabs.tabCloseRequested.connect(self.closeTab)
-        
 
-        #
         self.tabs.setUsesScrollButtons(True)
-        ########## Create first tab
-        #self.tab[0].layout = QVBoxLayout(self)
-        #self.pushButton1 = QPushButton('PyQt5 button')
-        #self.tab[0].layout.addWidget(self.pushButton1)
-        #self.tab[0].setLayout(self.tab[0].layout)
+        # Create first tab
+        # self.tab[0].layout = QVBoxLayout(self)
+        # self.pushButton1 = QPushButton('PyQt5 button')
+        # self.tab[0].layout.addWidget(self.pushButton1)
+        # self.tab[0].setLayout(self.tab[0].layout)
 
         # Add tabs to widget
         self.layout.addWidget(self.add_button)
@@ -79,28 +88,25 @@ class PersephonepTableWidget(QWidget):
     def _addTab(self, index):
         ''' add Tab
         '''
-        # print(index)
-        self.tab.append(PersephonepWindow(parent = self))
-        self.tabs.addTab(self.tab[-1], '') # do not match tab index & tab num
+        self.tab.append(PersephonepWindow(parent=self))
+        #  do not match tab index & tab num
+        self.tabs.addTab(self.tab[-1], '')
         self.tabs.setTabText(index, 'VanilaPage')
-        # self.tab[-1].window.titleChanged.connect(lambda: self.tabs.setTabText(self.tabs.currentIndex(), self.tab[self.tabs.currentIndex()].window.title())) # this is too long. So, replace this sentence with below code.
         self.tab[-1].window.titleChanged.connect(self.updateTabName)
-        # self.tab[index-1].window.urlChanged.connect(lambda: self.tabs.setTabText(index, self.tab[index-1].window.title())) # too fast to get page title
-        # print('addTab', index, self.tabs.currentIndex())
-        
+
     def closeTab(self, index):
-        ''' close Tab. 
+        ''' close Tab.
         '''
-        # print(index)
-        widget = self.tabs.widget(index)
+        # widget = self.tabs.widget(index)
         self.tab.pop(index)
         self.tabs.removeTab(index)
 
     def updateTabName(self):
         ''' re-set tab name
         '''
-        self.tabs.setTabText(self.tabs.currentIndex(), self.tab[self.tabs.currentIndex()].window.title())
-        
+        self.tabs.setTabText(self.tabs.currentIndex(),
+                             self.tab[self.tabs.currentIndex()].window.title())
+
     def center(self):
         ''' centering widget
         '''
@@ -109,26 +115,25 @@ class PersephonepTableWidget(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-
-        
     @pyqtSlot()
     def on_click(self):
         print("\n")
         for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+            print(currentQTableWidgetItem.row(),
+                  currentQTableWidgetItem.column(),
+                  currentQTableWidgetItem.text())
 
-    
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     # setWindowIcon is a method for QApplication, not for QWidget
-    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'icon_persephone.png')
+    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__),
+                        'icon_persephone.png')
     app.setWindowIcon(QIcon(path))
-
 
     # app.setApplicationName('IE')
     # app.setApplicationVersion('1.0')
-    
-    
+
     ui = PersephonepMainWidget()
     sys.exit(app.exec_())
