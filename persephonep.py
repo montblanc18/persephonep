341c43485f81d8c2e8638a1fa6352118ea8e0ecc
montblanc18 <montblanc.u3.07@gmail.com>
2019-06-15 15:22:11 +0900

    Start to revise this browser.

--- persephonep.py
+++ persephonep.py
@@ -4,23 +4,34 @@
 
 import sys
 import os
+import argparse
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QPushButton,
                              QTabWidget, QApplication,
-                             # QHBoxLayout, QGridLayout,
                              QVBoxLayout, QLabel, QDesktopWidget)
 from PyQt5.QtGui import QIcon
-# from func_persephonep import *
 from func_persephonep import PersephonepWindow
 from PyQt5.QtCore import pyqtSlot
 
 __program__ = 'PERSEPHONEP'
 
+def persephonep_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action = 'store_true',help='Show debug messages.')
+    parser.add_argument('-u', '--url', type = str, default = 'http://www.google.com', help='Set url you want to open at start.')
+    parser.add_argument('-v', '--verbose', action = 'store_true', help ='Show details of this browser.')
+    args = parser.parse_args()
+    if args.debug: print(args)
+    return args
+
+def create_main_window():
+    main_window = PersephonepMainWindow()
+    main_window.show()
+    return main_window
+
 ''' This is a main window of the browser.
     This class includes PersephonepMainWidget.
 '''
-
-
-class PersephonepMainWidget(QMainWindow):
+class PersephonepMainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -33,8 +44,6 @@ class PersephonepMainWidget(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.table_widget = PersephonepTableWidget(self)
         self.setCentralWidget(self.table_widget)
-
-        self.show()
 
 
 ''' This is a Tab Handle Class for this browser.
@@ -125,15 +134,34 @@ class PersephonepTableWidget(QWidget):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
 
+    ''' parse options 
+    '''
+    args = persephonep_parser()
+
+    app = QApplication(sys.argv)
     # setWindowIcon is a method for QApplication, not for QWidget
     path = os.path.join(os.path.dirname(sys.modules[__name__].__file__),
                         'icon_persephone.png')
     app.setWindowIcon(QIcon(path))
+    
+    ''' Show Windows
+    '''
+    main_window = create_main_window()
+    
+    ''' ToDo: Open URLs which you set by args
+    '''
 
-    # app.setApplicationName('IE')
-    # app.setApplicationVersion('1.0')
+    ''' Call event loop
+    Notice: 
+        QApplication manages events, resources, and so on.
+        PyQt Application needs this object.
+        ```QApplication.exec_()```, ```app.exec_()``` in this program, calls event loop.
+        By executing this sentence, program hands management of resources to PyQt.
+    '''
+    exit_code = app.exec_()
 
-    ui = PersephonepMainWidget()
-    sys.exit(app.exec_())
+    ''' ToDo: Write save data like setting before finishing program
+    '''
+
+    sys.exit(exit_code)
