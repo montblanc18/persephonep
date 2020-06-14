e3fbada5dc24c898996bdb67c7b4111f2f7eab1f
montblanc18 <skurita.phys@gmail.com>
2020-06-14 14:06:13 +0900

    introducing pytest

--- tests/test_persephonep.py
+++ tests/test_persephonep.py
@@ -2,26 +2,38 @@
 # coding: utf-8
 #
 
+import pytest
 import unittest
-from persephonep import *
+import sys
+import os
+import argparse
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QPushButton,
+                             QTabWidget, QApplication,
+                             QVBoxLayout, QLabel, QDesktopWidget, QStatusBar)
+from PyQt5.QtGui import QIcon
 import PyQt5.QtCore
 
+from persephonep import * 
+from func_persephonep import *
 
 def test_add_button(qtbot):
+    return 0
+'''
     app = QApplication(sys.argv)
 
     # setWindowIcon is a method for QApplication, not for QWidget
     path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'icon_persephone.png')
     app.setWindowIcon(QIcon(path))
 
-    ui = PersephonepMainWidget()
+    # ui = PersephonepMainWindow()
+    ui = create_main_window(app)
 
     qWarning('test')
 
     qtbot.addWidget(ui)
     qtbot.mouseClick(ui.table_widget.add_button, PyQt5.QtCore.Qt.LeftButton)
     assert ui.table_widget.add_button.text() == '+'
-
+'''
 
 
 ''''
